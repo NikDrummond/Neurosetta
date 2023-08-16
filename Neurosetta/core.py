bbe148c4afd0e5ab0bdfc044ff088b194f5c0c53
Nik Drummond <nikolasdrummond@gmail.com>
2023-08-16 13:25:58 +0200

    Simple core build bits

--- Neurosetta/core.py
+++ Neurosetta/core.py
@@ -1,75 +1,190 @@
-import numpy as np
+import os
+
 import graph_tool.all as gt
-import vaex as vx 
+import numpy as np
+import pandas as pd
 
 # Main core class
 
 class Stone(object):
-    """
-    Core class
-    """
-    
-    def __init__(self, name, id, units):
+
+    # Constructor
+    def __init__(self, name):
         self.name = name
         self.id = id
-        self.units = units
-# Child Graph class
 
-class Graph(Stone):
-    """
-    Core graph class
-    """
-    def __init__(self,name,id,dimensions):
-        super().__init__(name,id,dimensions)
+class Tree_graph(Stone):
 
-
-# Child Image Class
-
-class Image(Stone):
-    """
-    Core Image object
-    """
-    
-    def __init__(self, name, id, dimensions):
-        super().__init__(name,id)
-        self.dimensions = dimensions
-
-# Grand-child graph classes
-
-class neu_Tree(Graph):
-    """
-    Tree graph representation of a neuron based on graph_tool.Graph object
-    """
-
-    def __init__(self,name,id,dimensions,graph):
-        super().__init__(name,id, dimensions)
+    def __init__(self,name,graph):
         self.graph = graph
 
-class neu_Mesh(Graph):
-    """
-    Mesh neuron representation
-    """
+        super().__init__(name)
 
-    def __init__(self,name,id,dimensions,mesh):
-        super().__init__(name,id,dimensions)
+class Node_table(Stone):
+
+    def __init__(self,name,nodes):
+        self.nodes = nodes
+
+        super().__init__(name)
+
+class Neuron_mesh(Stone):
+
+    def __init__(self,name,mesh):
         self.mesh = mesh
 
-# Grand-child image classes
+        super().__init__(name)
 
-class neu_denseImage(Image):
+# read swc
+def read_swc(file_path,output = 'Table'):
     """
-    Dense image representation
-    """
-
-    def __init__(self,name,id,image, dimensions):
-        super().__init__(name,id, dimensions)
-        self.image = image
-
-class neu_sparseImage(Image):
-    """
-    Spare image representation
+    Read in swc file and return table/graph output
     """
 
-    def __init__(self,name,id,image, dimensions): 
-        super().__init__(name,id,dimensions)
-        self.image = image
+    name = os.path.splitext(os.path.basename(file_path))[0]
+    df = table_from_swc(file_path)
+
+    if output == 'Table':
+        return Node_table(name = name, nodes = df)
+
+    elif output == 'Graph':
+        g = graph_from_table(df)
+        return Tree_graph(name = name, graph = g)
+
+# table from swc
+def table_from_swc(file_path):
+    """
+    Node table from swc file, using pandas
+    """
+    df = pd.read_csv(file_path, 
+            names = ['node_id','type','x','y','z','radius','parent_id'],
+            comment = '#',
+            engine = 'c',
+            delim_whitespace = True,
+            dtype = {'node_id':np.int32,
+                    'type':np.int32,
+                    'x':np.float64,
+                    'y':np.float64,
+                    'z':np.float64,
+                    'radius':np.float64,
+                    'parent_id':np.int32})
+
+    # check for duplicate node ids
+    if not np.unique(df.node_id.values).size == len(df.node_id.values):
+        raise AttributeError('Duplicate Node Ids found')
+
+    return df
+
+# Graph from table
+def _node_inds(g,df):
+    """
+    Given a graph, with ids as a vp, and a df with the same set of ides, find the order of indicies to order things going from the table to the graph
+
+    This is important whenver adding an attribute to nodes in a graph
+    """
+        # node id orders - this is the order of nodes in the graph, which match the node ids in the table
+    ids = g.vp['ids'].a
+    # nodes in the table
+    nodes = df['node_id'].values
+
+    inds = [np.where(nodes == i)[0][0] for i in ids]
+
+    return inds
+
+def graph_from_table(df):
+
+    if isinstance(df,Node_table):
+        df = df.nodes.copy()
+    elif isinstance(df,pd.DataFrame):
+        pass
+    else:
+        raise AttributeError('Input type not recognised')
+    # get edges (without root)
+    edges = df.loc[df.parent_id != -1,['parent_id','node_id']].values
+
+    # create new (hashed) graph with edges
+    g = gt.Graph(edges,hashed = True,hash_type = 'int')
+
+    # we want to know the indicies of the nodes in the table that match how the nodes were added to the graph
+
+    # indicies of graph nodes in table
+    inds = _node_inds(g,df)
+
+    # add some attributed from node table
+    # initilise vertex properties - radius, coordinates
+    vprop_rad = g.new_vp('double')
+    vprop_coords = g.new_vp('vector<double>')
+
+    # populate them - i think the error is here??
+    vprop_rad.a = df.radius.values[inds]
+    vprop_coords.set_2d_array(df[['x','y','z']].values[inds].T)
+
+    # add type to nodes - infer from topology rather than from table
+     # types
+    out_deg = g.get_out_degrees(g.get_vertices())
+    in_deg = g.get_in_degrees(g.get_vertices())
+    ends = np.where(out_deg == 0)
+    branches = np.where(out_deg > 1)
+    root = np.where(in_deg == 0)
+    node_types = np.zeros_like(g.get_vertices())
+    node_types[ends] = 6
+    node_types[branches] = 5
+    node_types[root] = -1
+
+    # create and add populate property
+    vprop_type = g.new_vp('int')
+    vprop_type.a = node_types
+    g.vp['type'] = vprop_type
+
+    # add them
+    g.vp['radius'] = vprop_rad
+    g.vp['coordinates'] = vprop_coords
+
+    return g
+
+# graph to node table
+def graph_to_table(g, output = 'Table'):
+    """
+    Convert Tree Graph to swc like table
+    """
+
+    # if Tree Graph
+    if isinstance(g,Tree_graph):
+        name = g.name
+        g = g.graph
+    elif isinstance(g,gt.Graph):
+        name = None
+    else:
+        raise AttributeError("input type not supported")
+
+    # generate a node table from a graph.
+    # node ids
+    ids = g.vp['ids'].a
+    radius = g.vp['radius'].a
+    # is there a better way to do this??
+    coords = np.asarray([g.vp['coordinates'][i] for i in g.iter_vertices()])
+    # generate parent node ids
+    parents = np.zeros_like(ids)
+    for i in range(len(ids)):
+        v = g.get_vertices()[i]
+        # get parent
+        p = g.get_in_neighbors(v)
+        if len(p) != 1:
+            p_id = -1
+        else:
+            p_id = g.vp['ids'][p[0]]
+        parents[i] = p_id
+
+    Type = g.vp['type'].a
+
+    df = pd.DataFrame({'node_id':ids,
+                'type':Type,
+                'x':coords[:,0],
+                'y':coords[:,1],
+                'z':coords[:,2],
+                'radius':radius,
+                'parent_id':parents}).sort_values('node_id').reset_index(drop = True)
+
+    if output == 'Neurosetta':
+        return Node_table(name = name, nodes = df)
+    elif output == 'Table':
+        return df
